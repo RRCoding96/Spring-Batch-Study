ea5fdda609029e694e32d22ab018c9b3d4f88c65
김윤욱 <reznov03048@gmail.com>
2024-02-23 21:43:10 +0900

    Job 설정 수정

--- src/main/java/io/springbatch/springbatch/config/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/config/JobConfiguration.java
@@ -1,6 +1,7 @@
 package io.springbatch.springbatch.config;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -25,8 +26,13 @@ public class JobConfiguration {
     public Step step1(JobRepository jobRepository, PlatformTransactionManager tx) {
         return new StepBuilder( "step1", jobRepository)
             .tasklet((contribution, chunkContext) -> {
+                JobInstance jobInstance = contribution.getStepExecution().getJobExecution().getJobInstance();
                 System.out.println("====================================");
                 System.out.println(" step1 executed ");
+                System.out.println("jobInstance.getId() : " + jobInstance.getId());
+                System.out.println("jobInstance.getInstanceId() : " + jobInstance.getInstanceId());
+                System.out.println("jobInstance.getJobName() : " + jobInstance.getJobName());
+                System.out.println("jobInstance.getJobVersion : " + jobInstance.getVersion());
                 System.out.println("====================================");
                 return RepeatStatus.FINISHED;
             }, tx)
