54e92142039544fee8071a1c8bd24f0e0a5f9a43
김윤욱 <reznov03048@gmail.com>
2024-02-26 22:00:30 +0900

    ExecutionContext

---
+++ src/main/java/io/springbatch/springbatch/config/ExecutionContextConfiguration.java
@@ -0,0 +1,74 @@
+package io.springbatch.springbatch.config;
+
+import io.springbatch.springbatch.tasklet.ExecutionContextTasklet1;
+import io.springbatch.springbatch.tasklet.ExecutionContextTasklet2;
+import io.springbatch.springbatch.tasklet.ExecutionContextTasklet3;
+import io.springbatch.springbatch.tasklet.ExecutionContextTasklet4;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Date;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExecutionContextConfiguration {
+
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
+
+    @Bean
+    public Job job(
+        JobRepository jobRepository,
+        Step step1,
+        Step step2,
+        Step step3,
+        Step step4
+    ) {
+        return new JobBuilder("Job", jobRepository)
+            .start(step1)
+            .next(step2)
+            .next(step3)
+            .next(step4)
+            .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager tx) {
+        return new StepBuilder( "step1", jobRepository)
+            .tasklet(executionContextTasklet1, tx)
+            .build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager tx) {
+        return new StepBuilder( "step2", jobRepository)
+            .tasklet(executionContextTasklet2, tx)
+            .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager tx) {
+        return new StepBuilder( "step3", jobRepository)
+            .tasklet(executionContextTasklet3, tx)
+            .build();
+    }
+
+    @Bean
+    public Step step4(JobRepository jobRepository, PlatformTransactionManager tx) {
+        return new StepBuilder( "step4", jobRepository)
+            .tasklet(executionContextTasklet4, tx)
+            .build();
+    }
+}
